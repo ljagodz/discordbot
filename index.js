33a887e49ae6d377a83c8f509c507e593af5cf7d
ljagodz <ljagodz@gmail.com>
2020-02-18 23:38:49 -0800

    added more simple commands

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -6,11 +6,17 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login(config.token);
+client.login(token);
 
 // listen for messages:
 client.on('message', message => {
-	if(message.content === '!ping') {
+	if(message.content.startsWith(`${prefix}ping`)) {
 		message.channel.send('Pong.');
+	} else if (message.content.startsWith(`${prefix}beep`)) {
+		message.channel.send('Boop.');
+	} else if (message.content === `${prefix}server`) {
+		message.channel.send(`This server's name is: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+	} else if (message.content === `${prefix}user-info`) {
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
 	}
 });
