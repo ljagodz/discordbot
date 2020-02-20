c258e9759a7fa6e71c9b4a9c65b3ab5a784fc878
ljagodz <ljagodz@gmail.com>
2020-02-19 21:08:49 -0800

    added expected command usage example in args-info command

---
+++ commands/kick.js
@@ -0,0 +1,16 @@
+module.exports = {
+	name: 'kick',
+	description: 'Kick the selected user.',
+	args: true,
+	usage: '<user>',
+	execute(message, args) {
+
+		if(!message.mentions.users.size) {
+			return message.reply('you need to tag a user in order to kick them!');
+		}
+
+		const taggedUser = message.mentions.users.first();
+		message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+	}
+};
+
